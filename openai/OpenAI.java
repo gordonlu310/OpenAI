9fa8323afa9c8ba598620202a3dc60006afaeaff
Gordon <88015331+GordonL0049@users.noreply.github.com>
2023-04-25 21:46:42 +0800

    Update OpenAI.java

--- openai/OpenAI.java
+++ openai/OpenAI.java
@@ -7,6 +7,7 @@ import com.google.appinventor.components.runtime.ComponentContainer;
 import com.google.appinventor.components.runtime.util.YailList;
 import com.google.appinventor.components.runtime.EventDispatcher;
 import com.google.appinventor.components.runtime.util.*;
+import com.google.appinventor.components.runtime.util.YailDictionary;
 
 import java.io.*;
 import java.util.*;
@@ -15,18 +16,19 @@ import java.lang.Exception;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
-import java.net.URL;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
 
 import org.json.JSONObject;
 import org.json.JSONArray;
 
-import com.gordonlu.openai.helpers.Model;
 import com.gordonlu.openai.helpers.Size;
 
-@DesignerComponent(version = 3, description = "An extension that allows you to use AI and connect with OpenAI. Created by Gordon.", iconName = "aiwebres/icon.png",
+@DesignerComponent(version = 4, description = "An extension that allows you to use AI and connect with OpenAI. Created by Gordon.", iconName = "aiwebres/icon.png",
  category = ComponentCategory.EXTENSION, nonVisible = true)
 @UsesLibraries(libraries = "json-20220924.jar")
 @SimpleObject(external = true)
@@ -37,71 +39,99 @@ public class OpenAI extends AndroidNonvisibleComponent {
         super(container.$form());
     }
 
+    HashMap<String, String> models = new HashMap<String, String>() {{
+        put("A1", "gpt-3.5-turbo");
+        put("A2", "gpt-3.5-turbo-0301");
+        put("B1", "gpt-4");
+        put("B2", "gpt-4-0314");
+        put("B3", "gpt-4-32k");
+        put("B4", "gpt-4-32k-0314");
+    }};
+
     @SimpleFunction(description = "Chats with the OpenAI bot, with your query and your API key. Choose a model to talk to with the model parameter." +
-     " The temperature parameter decides the type of answer that the bot will give you. Try values closer to 1 for creative answers, and values closer to 0" + 
-     " for informative answers. The temperature parameter MUST be a value between 0 and 1 inclusively.") 
-    public void Chat(final String prompt, @Options(Model.class) final String model, final String apiKey, final int maxTokens, final double temperature) {
+     " Higher values for the temperature parameter like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic." + 
+     " The temperature MUST be between 0 and 2 inclusively.\nStarting from version 4, you must input a model code instead of the model name from the OpenAI documentation." + 
+     " A model code corresponds to a model name. Find a list of model codes and their corresponding model names in the documentation.") 
+    public void Chat(final String prompt, final String model, final String apiKey, final int maxTokens, final double temperature) {
 		AsynchUtil.runAsynchronously(new Runnable() {
             @Override
             public void run () {
                 try {
-                    final String mod = model;
-                    URL url = new URL("https://api.openai.com/v1/completions");
-                    HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
-                    httpConn.setRequestMethod("POST");
-                    httpConn.setRequestProperty("Content-Type", "application/json");
-                    httpConn.setRequestProperty("Authorization", "Bearer " + apiKey);
-                    httpConn.setDoOutput(true);
-                    OutputStreamWriter writer = new OutputStreamWriter(httpConn.getOutputStream());
-                    JSONObject data = new JSONObject();
-                    data.put("model", mod);
-                    data.put("prompt", prompt.replace("\"", "'"));
-                    data.put("temperature", temperature);
-                    data.put("max_tokens", maxTokens);
-                    writer.write(data.toString());
-                    writer.flush();
-                    writer.close();
-                    httpConn.getOutputStream().close();
-                    InputStream responseStream = httpConn.getResponseCode() / 100 == 2
-                            ? httpConn.getInputStream()
-                            : httpConn.getErrorStream();
-                    BufferedReader in = new BufferedReader(new InputStreamReader(responseStream));
-                    String inputLine;
-                    final StringBuilder response = new StringBuilder();
-                    while ((inputLine = in.readLine()) != null) {
-                        response.append(inputLine);
-                    }
-                    in.close();
-                    form.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                String json = response.toString();
-                                JSONObject obj = new JSONObject(json);
-                                if (obj.has("error")) {
-                                    String output = obj.getJSONObject("error").getString("message");
-                                    Error(output, "Chat");
-                                } else if (obj.has("choices")) {
-                                    String output = obj.getJSONArray("choices").getJSONObject(0).getString("text");
-                                    int usage = obj.getJSONObject("usage").getInt("total_tokens");
-                                    if (output.startsWith("\n\n")) {
-                                        output = output.substring(2);
-                                    }
-                                    RespondedToChat(output, usage);
-                                } else {
-                                    Error("The process is completed but nothing is returned. The output is:\n" + json, "Chat");
-                                }
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                                Error(e.getMessage(), "Chat");
-                            }
+                    if (models.get(model) == null) {
+                        Error("No such model found for model code " + model + ". All possible codes are A1, A2, B1, B2, B3 and B4. Check the documentation for a list" + 
+                        " of possible model codes that correspond with an OpenAI model.", "Chat");
+                    } else {
+                        final String mod = model;
+                        URL url = new URL("https://api.openai.com/v1/chat/completions");
+                        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+                        httpConn.setRequestMethod("POST");
+                        httpConn.setRequestProperty("Content-Type", "application/json");
+                        httpConn.setRequestProperty("Authorization", "Bearer " + apiKey);
+                        httpConn.setDoOutput(true);
+                        OutputStreamWriter writer = new OutputStreamWriter(httpConn.getOutputStream());
+    
+                        JSONObject messageDict = new JSONObject();
+                        messageDict.put("role", "user");
+                        messageDict.put("content", prompt.replace("\"", "'"));
+    
+                        JSONArray message = new JSONArray();
+                        message.put(messageDict);
+    
+                        JSONObject data = new JSONObject();
+                        data.put("model", models.get(mod));
+                        data.put("messages", message);
+                        data.put("temperature", temperature);
+                        data.put("max_tokens", maxTokens);
+    
+                        writer.write(data.toString());
+                        writer.flush();
+                        writer.close();
+                        httpConn.getOutputStream().close();
+                        InputStream responseStream = httpConn.getResponseCode() / 100 == 2
+                                ? httpConn.getInputStream()
+                                : httpConn.getErrorStream();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(responseStream));
+                        String inputLine;
+                        final StringBuilder response = new StringBuilder();
+                        while ((inputLine = in.readLine()) != null) {
+                            response.append(inputLine);
                         }
-                    });
-                } catch (Exception e) {
+                        in.close();
+    
+                        form.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    String json = response.toString();
+                                    JSONObject obj = new JSONObject(json);
+                                    if (obj.has("error")) {
+                                        String output = obj.getJSONObject("error").getString("message");
+                                        Error(output, "Chat");
+                                    } else if (obj.has("choices")) {
+                                        String output = obj.getJSONArray("choices").getJSONObject(0).getJSONObject("message").getString("content");
+                                        int usage = obj.getJSONObject("usage").getInt("total_tokens");
+                                        if (output.startsWith("\n\n")) {
+                                            output = output.substring(2);
+                                        } else if (output.startsWith("\n")) {
+                                            output = output.substring(1);
+                                        }
+    
+                                        RespondedToChat(output, usage);
+                                    } else {
+                                        Error("The process is completed but nothing is returned. The output is:\n" + json, "Chat");
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                    Error(e.getMessage(), "Chat");
+                                }
+                            }
+                        });
+                    }
+                }  catch (Exception e) {
                     e.printStackTrace();
                     Error(e.getMessage(), "Chat");
                 }
-            }
+            }  
         });
     }
 
@@ -119,18 +149,20 @@ public class OpenAI extends AndroidNonvisibleComponent {
                     httpConn.setRequestProperty("Authorization", "Bearer " + apiKey);
                     httpConn.setDoOutput(true);
                     OutputStreamWriter writer = new OutputStreamWriter(httpConn.getOutputStream());
+
                     JSONObject data = new JSONObject();
-                    data.put("size", size);
-                    data.put("n", 1);
                     data.put("prompt", prompt.replace("\"", "'"));
+                    data.put("size", size);
                     writer.write(data.toString());
                     writer.flush();
                     writer.close();
+
                     httpConn.getOutputStream().close();
                     InputStream responseStream = httpConn.getResponseCode() / 100 == 2
                             ? httpConn.getInputStream()
                             : httpConn.getErrorStream();
                     BufferedReader in = new BufferedReader(new InputStreamReader(responseStream));
+
                     String inputLine;
                     final StringBuilder response = new StringBuilder();
                     while ((inputLine = in.readLine()) != null) {
@@ -147,19 +179,20 @@ public class OpenAI extends AndroidNonvisibleComponent {
                                     String output = obj.getJSONArray("data").getJSONObject(0).getString("url");
                                     GeneratedImage(output);
                                 } else if (obj.has("error")) {
-                                    Error(obj.getJSONObject("error").getString("message"), "GenerateImage");
+                                    Error(obj.getJSONObject("error").getString("message"), "GeneratedImage");
                                 } else {
-                                    Error("The process is completed but nothing is returned. The output is:\n" + json, "GenerateImage");
+                                    Error("The process is completed but nothing is returned. The output is:\n" + json, "GeneratedImage");
                                 }
                             } catch (Exception e) {
                                 e.printStackTrace();
-                                Error(e.getMessage(), "GenerateImage");
+                                Error(e.getMessage(), "GeneratedImage");
                             }
                         }
                     });
+                    
                 } catch (Exception e) {
                     e.printStackTrace();
-                    Error(e.getMessage(), "GenerateImage");
+                    Error(e.getMessage(), "GeneratedImage");
                 }
             }
         });
@@ -172,7 +205,7 @@ public class OpenAI extends AndroidNonvisibleComponent {
 
     @SimpleEvent(description = "This event is fired when OpenAI has generated an image of your choice!")
     public void GeneratedImage(String imageUrl) {
-        EventDispatcher.dispatchEvent(this, "GotImage", imageUrl);
+        EventDispatcher.dispatchEvent(this, "GeneratedImage", imageUrl);
     }
 
     @SimpleEvent(description = "This error is fired when an error has occurred. The block parameter tells you which block has fired the event.")
@@ -253,5 +286,96 @@ public class OpenAI extends AndroidNonvisibleComponent {
    @SimpleEvent(description = "This event is fired when OpenAI has edited your input from the EditText block!")
    public void EditedInput(String output, int tokensSpent) {
        EventDispatcher.dispatchEvent(this, "EditedInput", output, tokensSpent);
+   }
+
+   // blocks added for version 4
+
+   @SimpleFunction(description = "The moderation API is a service provided by OpenAI that allows you to check whether a text contains inappropriate or offensive information. " + 
+   "Put the text that you want to analyze in the input parameter, and supply the block with an API key.")
+   public void ModerateText(final String input, final String apiKey) {
+    AsynchUtil.runAsynchronously(new Runnable() {
+        @Override
+        public void run () {
+            try {
+                URL url = new URL("https://api.openai.com/v1/moderations");
+                HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+                httpConn.setRequestMethod("POST");
+                httpConn.setRequestProperty("Content-Type", "application/json");
+                httpConn.setRequestProperty("Authorization", "Bearer " + apiKey);
+                httpConn.setDoOutput(true);
+                OutputStreamWriter writer = new OutputStreamWriter(httpConn.getOutputStream());
+                JSONObject data = new JSONObject();
+                data.put("input", input.replace("\"", "'"));
+                writer.write(data.toString());
+                writer.flush();
+                writer.close();
+                httpConn.getOutputStream().close();
+                InputStream responseStream = httpConn.getResponseCode() / 100 == 2
+                        ? httpConn.getInputStream()
+                        : httpConn.getErrorStream();
+                BufferedReader in = new BufferedReader(new InputStreamReader(responseStream));
+                String inputLine;
+                final StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                form.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            String json = response.toString();
+                            JSONObject obj = new JSONObject(json);
+                            if (obj.has("error")) {
+                                String output = obj.getJSONObject("error").getString("message");
+                                Error(output, "Chat");
+                            } else if (obj.has("results")) {
+                                boolean flagged = obj.getJSONArray("results").getJSONObject(0).getBoolean("flagged");
+                                JSONObject categories = obj.getJSONArray("results").getJSONObject(0).getJSONObject("categories");
+                                JSONObject categoryScoresD = obj.getJSONArray("results").getJSONObject(0).getJSONObject("category_scores");
+                                YailDictionary categoryFlags = new YailDictionary();
+                                Iterator<String> keys = categories.keys();
+                                while (keys.hasNext()) {
+                                    String key = keys.next();
+                                    Object value = categories.get(key);
+                                    categoryFlags.put(key, value);
+                                }
+                                YailDictionary categoryScores = new YailDictionary();
+                                Iterator<String> keys2 = categoryScoresD.keys();
+                                while (keys2.hasNext()) {
+                                    String key = keys2.next();
+                                    Object value = categoryScoresD.get(key);
+                                    categoryScores.put(key, value);
+                                }
+                                ModeratedText(flagged, categoryFlags, categoryScores);
+                            } else {
+                                Error("The process is completed but nothing is returned. The output is:\n" + json, "Chat");
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Error(e.getMessage(), "Chat");
+                        }
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+                Error(e.getMessage(), "Chat");
+            }
+        }
+    });
+   }
+
+   @SimpleEvent(description = "This event is fired when OpenAI has moderated the text you inputted in the ModerateText block! 'flagged' refers to whether the text contains" + 
+   " indecent information in general.")
+   public void ModeratedText(boolean flagged, YailDictionary categoryFlags, YailDictionary categoryScores) {
+       EventDispatcher.dispatchEvent(this, "ModeratedText", flagged, categoryFlags, categoryScores);
+   }
+
+   public boolean stringToBoolean(String input) {
+    if (input == "true") {
+        return true;
+    } else {
+        return false;
+    }
    }
 }
